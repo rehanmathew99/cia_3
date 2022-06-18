5b6908f46fee0e299129976348bc07c17c7eed00
rehanmathew99 <105007030+rehanmathew99@users.noreply.github.com>
2022-06-18 10:24:55 +0530

    Add files via upload

---
+++ scratch_8.py
@@ -0,0 +1,14 @@
+def data():
+    f = open("data.txt", "a")
+    li=[name,phone_num,email]
+    f.write(li)
+    f.close()
+i=0
+while i==0:
+    name=str(input("Enter Name "))
+    phone_num=int(input("Enter Phone Number "))
+    email=str(input("Enter Email ID "))
+    i=int(input("to continue press 0 and to exit press any other value"))
+f = open("data.txt", "r")
+print(f.read())
+f.close()
